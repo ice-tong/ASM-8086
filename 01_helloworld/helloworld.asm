065496b60b076df6c44005969cee3474ccfeea7b
icetong <xych6@outlook.com>
2021-04-18 17:15:39 +0800

    style (helloworld.asm, one_digit_addition.asm) : add code indent for good-looking

--- 01_helloworld/helloworld.asm
+++ 01_helloworld/helloworld.asm
@@ -7,23 +7,25 @@ ASSUME CS:CODES, DS:DATAS
 
 
 DATAS SEGMENT
-    helloWord DB "hello world!", 10, '$'    ; 10: ascii of \n
+
+    helloWord DB "hello world!", '$'    ; String must be terminated by '$'.
+
 DATAS ENDS
 
 
 CODES SEGMENT
 
-START:
+    START:
 
-    MOV AX, DATAS   ; The symbols used below are an offset base on DS.
-    MOV DS, AX      ; Set the data segment address to DS indirectly by AX.
+        MOV AX, DATAS       ; The symbols used below are an offset base on DS.
+        MOV DS, AX          ; Set the data segment address to DS indirectly by AX.
 
-    LEA DX, helloWord   ; Move string pointer to DX.
-    MOV AH, 09H         ; INT 21h / AH=9 - DOS PRINT STRING
-    INT 21H             ; Output of a string at DS:DX. String must be terminated by '$'.
+        LEA DX, helloWord   ; Move string pointer to DX.
+        MOV AH, 09H         ; INT 21h / AH=9 - DOS PRINT STRING
+        INT 21H             ; Output of a string at DS:DX.
 
-    MOV AH, 4CH         ; INT 21h / AH=4Ch - DOS EXIT CODE
-    INT 21H             ; Return control to the operating system (stop program).
+        MOV AH, 4CH         ; INT 21h / AH=4Ch - DOS EXIT CODE
+        INT 21H             ; Return control to the operating system (stop program).
 
 CODES ENDS
 
