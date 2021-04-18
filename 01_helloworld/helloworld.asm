007d6ab72e447f81b119e0282f19d5ddf15f245a
icetong <xych6@outlook.com>
2021-04-18 15:21:07 +0800

    first commit

---
+++ 01_helloworld/helloworld.asm
@@ -0,0 +1,24 @@
+
+ASSUME CS:CODES, DS:DATAS
+
+DATAS SEGMENT
+    helloWord DB "hello world!", 10, '$'    ; 10: ascii of \n
+DATAS ENDS
+
+CODES SEGMENT
+
+START:
+
+    MOV AX, DATAS   ; The symbols used below are an offset base on DS.
+    MOV DS, AX      ; Set the data segment address to DS indirectly by AX.
+
+    LEA DX, helloWord   ; Move string pointer to DX.
+    MOV AH, 09H         ; INT 21h / AH=9 - DOS PRINT STRING
+    INT 21H             ; Output of a string at DS:DX. String must be terminated by '$'.
+
+    MOV AH, 4CH         ; INT 21h / AH=4Ch - DOS EXIT CODE
+    INT 21H             ; Return control to the operating system (stop program).
+
+CODES ENDS
+
+    END START
